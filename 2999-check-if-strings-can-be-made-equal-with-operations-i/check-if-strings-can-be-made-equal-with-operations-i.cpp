77c47a8981da9741af45ef1a25df8b2fef99ce9a
Aritra Roy Choudhury <arc.nbg11@gmail.com>
2026-08-08 08:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (26.12%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        return ((s1[0] == s2[0] && s1[2] == s2[2]) ||
-                (s1[0] == s2[2] && s1[2] == s2[0])) &&
-               ((s1[1] == s2[1] && s1[3] == s2[3]) ||
-                (s1[1] == s2[3] && s1[3] == s2[1]));
+        string even1, even2;
+        string odd1, odd2;
+
+        for (int i = 0; i < 4; i++) {
+            if (i % 2 == 0) {
+                even1 += s1[i];
+                even2 += s2[i];
+            } else {
+                odd1 += s1[i];
+                odd2 += s2[i];
+            }
+        }
+
+        sort(even1.begin(), even1.end());
+        sort(even2.begin(), even2.end());
+
+        sort(odd1.begin(), odd1.end());
+        sort(odd2.begin(), odd2.end());
+
+        return even1 == even2 && odd1 == odd2;
     }
 };
